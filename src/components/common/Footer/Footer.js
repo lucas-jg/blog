35fc61b967b51441f41274cc9f27e1be89ca0913
jkkim <jkkim@comtrue.com>
2019-01-07 17:34:22 +0900

    Creat Footer component

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -1,16 +1,17 @@
-import React from 'react';
-
-import styles from './Footer.scss';
-import classNames from 'classnames/bind';
+import React from "react";
+import styles from "./Footer.scss";
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+  <footer className={cx("footer")}>
+    <Link to="/" className={cx("brand")}>
+      YS Blog
+    </Link>
+    <div className={cx("admin-login")}>관리자 로그인</div>
+  </footer>
 );
-
 
 export default Footer;
