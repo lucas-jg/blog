50c39da7cc86172730474d3404d0bd13d9119b04
jkkim <jkkim@comtrue.com>
2019-01-09 16:31:20 +0900

    Create editor page
    + Template
    + Header
    + Pane

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,7 +1,10 @@
 import React from "react";
+import EditorTemplate from "components/editor/EditorTemplate";
+import EditorHeader from "components/editor/EditorHeader";
+import EditorPane from "components/editor/EditorPane";
 
 const EditorPage = () => {
-  return <div>EditorPage</div>;
+  return <EditorTemplate header={<EditorHeader />} editor={<EditorPane />} preview="preview" />;
 };
 
 export default EditorPage;
