e2e5b25195c57aa48f48bc6c03ce9f069a0dcecd
jkkim <jkkim@comtrue.com>
2019-01-09 16:37:51 +0900

    Update editor page
    + PreviewPane

--- src/components/editor/PreviewPane/PreviewPane.js
+++ src/components/editor/PreviewPane/PreviewPane.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import styles from './PreviewPane.scss';
-import classNames from 'classnames/bind';
+import styles from "./PreviewPane.scss";
+import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
-
 const PreviewPane = () => (
-  <div>
-    PreviewPane
+  <div className={cx("preview-pane")}>
+    <h1 className={cx("title")}>제목</h1>
+    <div>내용</div>
   </div>
 );
-
 
 export default PreviewPane;
--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -2,9 +2,12 @@ import React from "react";
 import EditorTemplate from "components/editor/EditorTemplate";
 import EditorHeader from "components/editor/EditorHeader";
 import EditorPane from "components/editor/EditorPane";
+import PreviewPane from "components/editor/PreviewPane";
 
 const EditorPage = () => {
-  return <EditorTemplate header={<EditorHeader />} editor={<EditorPane />} preview="preview" />;
+  return (
+    <EditorTemplate header={<EditorHeader />} editor={<EditorPane />} preview={<PreviewPane />} />
+  );
 };
 
 export default EditorPage;
