450fb524514f5d57aef4d58ce70ea052e96eca88
jkkim <jkkim@comtrue.com>
2019-01-10 13:51:59 +0900

    Add markdownRender compnent

--- src/components/editor/PreviewPane/PreviewPane.js
+++ src/components/editor/PreviewPane/PreviewPane.js
@@ -2,13 +2,16 @@ import React from "react";
 
 import styles from "./PreviewPane.scss";
 import classNames from "classnames/bind";
+import MarkdownRender from "components/common/MarkdownRender";
 
 const cx = classNames.bind(styles);
 
-const PreviewPane = () => (
+const PreviewPane = ({ markdown, title }) => (
   <div className={cx("preview-pane")}>
-    <h1 className={cx("title")}>제목</h1>
-    <div>내용</div>
+    <h1 className={cx("title")}>{title}</h1>
+    <div>
+      <MarkdownRender markdown={markdown} />
+    </div>
   </div>
 );
 
