96a4093ec1b3c270c3c28abd7b1db9ce84d3be39
jkkim <jkkim@comtrue.com>
2019-01-11 17:31:48 +0900

    remove pender

--- src/store/modules/editor.js
+++ src/store/modules/editor.js
@@ -1,7 +1,6 @@
 import { createAction, handleActions } from "redux-actions";
 
 import { Map } from "immutable";
-import { pender } from "redux-pender";
 
 // action types
 const INITIALIZE = "editor/INITIALIZE";
