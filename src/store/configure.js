9e588468f3da1f4d1e1269aa42d1c0ac56b06c1b
Lucas <kjk727@me.com>
2019-01-17 14:02:08 +0900

    Pender를 불러오지 못한 문제 수정, 오타

--- src/store/configure.js
+++ src/store/configure.js
@@ -12,6 +12,6 @@ const composeEnhancers = devtools || compose;
 
 // preloadedState는 추후 서버사이드 렌더링을 했을 때 전달받는 초기 상태입니다.
 const configure = preloadedState =>
-  createStore(reducers, preloadedState, composeEnhancers(applyMiddleware(...middlewares)));
+	createStore(reducers, preloadedState, composeEnhancers(applyMiddleware(...middlewares)));
 
 export default configure;
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,4 +2,4 @@ export { default as base } from "./base";
 export { default as editor } from "./editor";
 export { default as list } from "./list";
 export { default as post } from "./post";
-export { penderreducer as pender } from "redux-pender";
+export { penderReducer as pender } from "redux-pender";
