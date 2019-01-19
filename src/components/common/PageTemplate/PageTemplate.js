46b95d1268d19095d6f25c666031e5cd97b69c4d
Lucas <kjk727@me.com>
2019-01-19 14:01:27 +0900

    포스트 수정하면서 에러 발생
    PATCH http://localhost:3000/api/posts/5c42ae159ac41d123cb2a712 404 (Not Found)

--- src/components/common/PageTemplate/PageTemplate.js
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -1,18 +1,17 @@
 import React from "react";
 import styles from "./PageTemplate.scss";
 import classNames from "classnames/bind";
-import Header from "components/common/Header";
 import HeaderContainer from "containers/common/HeaderContainer";
 import Footer from "components/common/Footer";
 
 const cx = classNames.bind(styles);
 
 const PageTemplate = ({ children }) => (
-	<div className={cx("page-template")}>
-		<HeaderContainer />
-		<main>{children}</main>
-		<Footer />
-	</div>
+    <div className={cx("page-template")}>
+        <HeaderContainer />
+        <main>{children}</main>
+        <Footer />
+    </div>
 );
 
 export default PageTemplate;
