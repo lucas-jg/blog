1bc90974ce923b15a01201dfe089e55583c170d1
Lucas <kjk727@me.com>
2019-01-19 14:42:44 +0900

    Add modal

---
+++ src/components/modal/AskRemoveModal/AskRemoveModal.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./AskRemoveModal.scss";
+import classNames from "classnames/bind";
+import ModalWrapper from "components/modal/ModalWrapper";
+
+const cx = classNames.bind(styles);
+
+const AskRemoveModal = () => <ModalWrapper>AskRemoveModal</ModalWrapper>;
+
+export default AskRemoveModal;
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PageTemplate from "components/common/PageTemplate";
 import PostContainer from "containers/post/PostContainer";
+import AskRemoveModal from "components/modal/AskRemoveModal";
 
 const PostPage = ({ match }) => {
-	const { id } = match.params;
-	return (
-		<PageTemplate>
-			<PostContainer id={id} />
-		</PageTemplate>
-	);
+    const { id } = match.params;
+    return (
+        <PageTemplate>
+            <PostContainer id={id} />
+            <AskRemoveModal />
+        </PageTemplate>
+    );
 };
 
 export default PostPage;
