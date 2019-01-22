e38f3c633255e5a53e365f6297ea49da57d36125
Lucas <kjk727@me.com>
2019-01-22 13:40:50 +0900

    모달창 애니메이션 적용

--- src/components/modal/AskRemoveModal/AskRemoveModal.js
+++ src/components/modal/AskRemoveModal/AskRemoveModal.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import styles from "./AskRemoveModal.scss";
 import classNames from "classnames/bind";
 import ModalWrapper from "components/modal/ModalWrapper";
@@ -11,7 +10,7 @@ const AskRemoveModal = ({ visible, onConfirm, onCancel }) => (
 	<ModalWrapper visible={visible}>
 		<div className={cx("question")}>
 			<div className={cx("title")}>포스트 삭제</div>
-			<div className={cx("description")}>이 포스틀르 정말로 삭제하시겠습니까?</div>
+			<div className={cx("description")}>이 포스트를 정말로 삭제하시겠습니까?</div>
 		</div>
 		<div className={cx("options")}>
 			<Button theme="gray" onClick={onCancel}>
--- src/components/modal/ModalWrapper/ModalWrapper.js
+++ src/components/modal/ModalWrapper/ModalWrapper.js
@@ -6,14 +6,37 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 class ModalWrapper extends Component {
+	state = {
+		animate: false
+	};
+
+	startAnimation = () => {
+		// animate 값을 true로 설정 후
+		this.setState({ animate: true });
+		setTimeout(() => {
+			this.setState({ animate: false });
+		}, 250);
+	};
+
+	componentDidUpdate = (prevProps, prevState) => {
+		if (prevProps.visible !== this.props.visible) {
+			this.startAnimation();
+		}
+	};
+
 	render() {
 		const { children, visible } = this.props;
-		if (!visible) return null;
+		const { animate } = this.state;
+
+		if (!visible && !animate) return null;
+
+		const animation = animate && (visible ? "enter" : "leave");
+
 		return (
 			<div>
-				<div className={cx("gray-background")} />
+				<div className={cx("gray-background", animation)} />
 				<div className={cx("modal-wrapper")}>
-					<div className={cx("modal")}>{children}</div>
+					<div className={cx("modal", animation)}>{children}</div>
 				</div>
 			</div>
 		);
