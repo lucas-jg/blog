4c4368997b1296410a3c128c78a3fc6b1866639c
Lucas <kjk727@me.com>
2019-01-24 21:24:25 +0900

    Create LoginModal

---
+++ src/components/modal/LoginModal/LoginModal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './LoginModal.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+
+const LoginModal = () => (
+  <div>
+    LoginModal
+  </div>
+);
+
+
+export default LoginModal;
