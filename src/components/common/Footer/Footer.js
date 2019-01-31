8f369213974eaaff60e410ad9b9096d56bfe7e0e
Lucas <kjk727@me.com>
2019-01-31 16:15:27 +0900

    Update FooterContainer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -5,15 +5,15 @@ import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
-const Footer = ({ onLoginClick }) => (
-    <footer className={cx("footer")}>
-        <Link to="/" className={cx("brand")}>
-            YS Blog
-        </Link>
-        <div className={cx("admin-login")} onClick={onLoginClick}>
-            관리자 로그인
-        </div>
-    </footer>
+const Footer = ({ onLoginClick, logged }) => (
+	<footer className={cx("footer")}>
+		<Link to="/" className={cx("brand")}>
+			YS Blog
+		</Link>
+		<div className={cx("admin-login")} onClick={onLoginClick}>
+			{logged ? "로그아웃" : "관리자 로그인"}
+		</div>
+	</footer>
 );
 
 export default Footer;
